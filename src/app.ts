62c0eace54985e0df0c4f5428320980a621845ff
Matheus Schneider <matheus.ssm@gmail.com>
2020-04-22 19:56:59 -0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -13,6 +14,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
